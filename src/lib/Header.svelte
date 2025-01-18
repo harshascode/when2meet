0b24a1f21b050e1d02c28e9cd630c8c9cc694475
harshascode <harshamesta0088@gmail.com>
2025-01-18 04:15:55 +0000

    Header has been added

---
+++ src/lib/Header.svelte
@@ -0,0 +1,20 @@
+<header class="border-b">
+	<div class="container flex h-14 items-center px-4">
+		<div class="flex gap-6 md:gap-10">
+			<a href="/" class="flex items-center space-x-2">
+				<span class="font-bold">When2meet</span>
+			</a>
+			<nav class="flex gap-6">
+				<a href="/about" class="text-sm font-medium transition-colors hover:text-primary">
+					About When2meet
+				</a>
+				<a
+					href="/new"
+					class="text-sm font-medium text-green-600 transition-colors hover:text-green-700"
+				>
+					Plan a New Event
+				</a>
+			</nav>
+		</div>
+	</div>
+</header>
